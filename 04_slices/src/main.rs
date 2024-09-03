8454f6e6b2a49597abb50236cd3acf1dafd1464f
Efe Özyay <gh@efe.cv>
2024-09-03 14:44:59 +0300

    Add slice exercises
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ 04_slices/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    println!("the first word is: {word}");
+    let word = first_word(&my_string[..]);
+    println!("the first word is: {word}");
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+    println!("the first word is: {word}");
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    println!("the first word is: {word}");
+    let word = first_word(&my_string_literal[..]);
+    println!("the first word is: {word}");
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!("the first word is: {word}");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
