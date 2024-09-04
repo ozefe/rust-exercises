936409d5540a2417f17ac9395478a01318c10899
Efe Özyay <gh@efe.cv>
2024-09-04 21:32:31 +0300

    Add enum exercises
    - Exercises on how to work with `match` and `enum`
    
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ 06_enums/src/main.rs
@@ -0,0 +1,88 @@
+#![allow(dead_code)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {}
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // ...
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let _home = IpAddr::V4(127, 0, 0, 1);
+    let _loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    println!("A US Penny is {}", value_in_cents(Coin::Penny));
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        // other => move_player(other),
+        _ => (), //reroll(),
+    }
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {max}"),
+        _ => (),
+    }
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    }
+}
+
+fn route(_ip_kind: IpAddrKind) {}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(_num_spaces: u8) {}
+fn reroll() {}
