a3b7f3d9f7fa7282226d4e9bbfa929ceb550803c
Efe Özyay <gh@efe.cv>
2024-09-07 01:00:05 +0300

    Add generics exercises
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ 10_generics/src/main.rs
@@ -0,0 +1,79 @@
+#![allow(dead_code)]
+
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item
+        }
+    }
+
+    largest
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {result}");
+    let result = largest(&number_list);
+    println!("The largest number is {result}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {result}");
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
+
+    let integer = Point { x: 5, y: 10 };
+    println!("integer.x = {}", integer.x());
+
+    let float: Point<f32> = Point { x: 1.0, y: 4.0 };
+    println!("Distance from origin: {}", float.distance_from_origin());
+
+    // Doesn't compile because both `x` and `y` fields of `Point` are generic over the same type `T`
+    // let wont_work = Point { x: 5, y: 4.0 };
+}
