078f9f3a374bff2b3e4db6547a62c7f479c54072
Efe Özyay <gh@efe.cv>
2024-09-07 02:08:21 +0300

    Add traits exercises
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ 10_traits/src/main.rs
@@ -0,0 +1,53 @@
+mod aggregator;
+use std::fmt::Display;
+
+use aggregator::{notify, NewsArticle, Summary, Tweet};
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn main() {
+    let tweet = returns_summarizable();
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best hockey team in the NHL.",
+        ),
+    };
+    // println!("New article available! {}", article.summarize());
+    notify(&article);
+
+    let pair = Pair::new('a', 'z');
+    pair.cmp_display();
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
