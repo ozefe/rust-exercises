5865b7617828a88f37b34c6c8a0df9475ca41272
Efe Özyay <gh@efe.cv>
2024-09-09 16:10:07 +0300

    Add `Box<T>` exercises
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ 15_box/src/main.rs
@@ -0,0 +1,16 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let b = Box::new(5);
+    println!("b = {b}");
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    dbg!(list);
+}
